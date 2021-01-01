a4c293ac7caca6552ecf8ec823aae48af88c062b
7rah <54563510+7rah@users.noreply.github.com>
2021-01-01 12:42:18 +0800

    Update acceptor.rs
    sniffing tls

--- src/protocol/dokodemo/acceptor.rs
+++ src/protocol/dokodemo/acceptor.rs
@@ -1,95 +1,75 @@
-use crate::protocol::{AcceptResult, Address, ProxyAcceptor, ProxyUdpStream, UdpRead, UdpWrite};
+use crate::error::Error;
+use crate::protocol::{AcceptResult, Address, DummyUdpStream, ProxyAcceptor};
 use async_trait::async_trait;
+use log;
 use serde::Deserialize;
-use smol::net::{TcpListener, TcpStream, UdpSocket};
+use smol::net::{TcpListener, TcpStream};
+use std::io;
 use std::io::Result;
-use std::str::FromStr;
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::str::{from_utf8, FromStr};
+use tls_parser::{
+    parse_tls_extension_sni, parse_tls_plaintext, TlsExtension, TlsMessage::Handshake,
+    TlsMessageHandshake::ClientHello,
+};
 
 #[derive(Deserialize)]
 pub struct DokodemoAcceptorConfig {
     listen_addr: String,
-    target_addr: String,
-}
-
-#[derive(Clone)]
-pub struct DokodemoUdpStream {
-    inner: UdpSocket,
-    addr: Address,
-}
-
-#[async_trait]
-impl UdpRead for DokodemoUdpStream {
-    async fn read_from(&mut self, buf: &mut [u8]) -> Result<(usize, Address)> {
-        let (size, _) = self.inner.recv_from(buf).await?;
-        Ok((size, self.addr.clone()))
-    }
-}
-
-#[async_trait]
-impl UdpWrite for DokodemoUdpStream {
-    async fn write_to(&mut self, buf: &[u8], _: &Address) -> Result<()> {
-        self.inner.send_to(buf, self.addr.to_string()).await?;
-        Ok(())
-    }
-}
-
-#[async_trait]
-impl ProxyUdpStream for DokodemoUdpStream {
-    type R = Self;
-    type W = Self;
-
-    fn split(self) -> (Self::R, Self::W) {
-        (self.clone(), self)
-    }
-
-    fn reunite(r: Self::R, _: Self::W) -> Self {
-        r
-    }
-
-    async fn close(self) -> Result<()> {
-        Ok(())
-    }
 }
 
 pub struct DokodemoAcceptor {
-    target_addr: Address,
-    udp_spawned: AtomicBool,
     tcp_listener: TcpListener,
+}
+
+#[inline]
+fn new_error<T: ToString>(message: T) -> io::Error {
+    return Error::new(format!("socks: {}", message.to_string())).into();
+}
+
+#[inline]
+fn parse_tls_connection(buf: &[u8]) -> io::Result<String> {
+    let (_, res) = parse_tls_plaintext(&buf).map_err(|_| new_error("unexpected protocol"))?;
+    match &res.msg[0] {
+        Handshake(ClientHello(contents)) => {
+            let ext = contents
+                .ext
+                .ok_or(())
+                .map_err(|_| new_error("unable to find tls extensions"))?;
+            match parse_tls_extension_sni(ext)
+                .map_err(|_| new_error("parse tls extensions error"))?
+            {
+                (_, TlsExtension::SNI(v)) => {
+                    let name = from_utf8(v[0].1).unwrap().to_string() + ":443";
+                    Ok(name)
+                }
+                _ => Err(new_error("can't find domain name")),
+            }
+        }
+        _ => Err(new_error("unexpected handshake type")),
+    }
 }
 
 #[async_trait]
 impl ProxyAcceptor for DokodemoAcceptor {
     type TS = TcpStream;
-    type US = DokodemoUdpStream;
+    type US = DummyUdpStream;
 
     async fn accept(&self) -> Result<AcceptResult<Self::TS, Self::US>> {
-        if !self.udp_spawned.load(Ordering::Relaxed) {
-            self.udp_spawned.store(true, Ordering::Relaxed);
-            let socket = UdpSocket::bind(self.tcp_listener.local_addr().unwrap()).await?;
-            let udp_stream = DokodemoUdpStream {
-                inner: socket,
-                addr: self.target_addr.clone(),
-            };
-            log::info!(
-                "udp socket listening on {}",
-                self.tcp_listener.local_addr().unwrap()
-            );
-            return Ok(AcceptResult::Udp(udp_stream));
-        }
         let (stream, addr) = self.tcp_listener.accept().await?;
         log::info!("tcp connection from {}", addr.to_string());
-        Ok(AcceptResult::Tcp((stream, self.target_addr.clone())))
+
+        let mut buf: [u8; 1024] = [0; 1024];
+        stream.peek(&mut buf).await?;
+        let name = parse_tls_connection(&buf)?;
+        log::info!("connected to {}", name);
+        let addr = Address::from_str(&name).map_err(|err| new_error(err.message))?;
+        Ok(AcceptResult::Tcp((stream, addr)))
     }
 }
 
 impl DokodemoAcceptor {
     pub async fn new(config: &DokodemoAcceptorConfig) -> Result<Self> {
         let tcp_listener = TcpListener::bind(config.listen_addr.clone()).await?;
-        Ok(DokodemoAcceptor {
-            target_addr: Address::from_str(&config.target_addr)?,
-            udp_spawned: AtomicBool::new(false),
-            tcp_listener,
-        })
+        Ok(DokodemoAcceptor { tcp_listener })
     }
 }
