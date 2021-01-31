a7c056391b6a8a6291f2a378d09461ef06bc3d42
zrah <zrah@protonmail.com>
2021-01-31 18:24:25 +0800

    添加路由功能

--- src/proxy/mod.rs
+++ src/proxy/mod.rs
@@ -1,5 +1,7 @@
 use crate::protocol::websocket::acceptor::{WebSocketAcceptor, WebSocketAcceptorConfig};
 use crate::protocol::websocket::connector::{WebSocketConnector, WebSocketConnectorConfig};
+use crate::protocol::Address;
+use crate::protocol::Address::{DomainNameAddress, SocketAddress};
 use crate::{
     error::Error,
     protocol::{
@@ -13,15 +15,20 @@ use crate::{
         ProxyTcpStream, ProxyUdpStream, UdpRead, UdpWrite,
     },
 };
+use bit_vec::BitVec;
 use futures::AsyncReadExt;
+use fxhash::hash32;
 use log::LevelFilter;
 use serde::Deserialize;
 use smol::future::FutureExt;
 use smol::io::{AsyncRead, AsyncWrite, AsyncWriteExt};
 use std::fs::File;
 use std::io;
+use std::io::BufRead;
 use std::io::Read;
+use std::path::Path;
 use std::sync::Arc;
+use std::hash::Hash;
 
 async fn copy_udp<R: UdpRead, W: UdpWrite>(r: &mut R, w: &mut W) -> io::Result<()> {
     let mut buf = [0u8; 1024 * 8];
@@ -105,6 +112,89 @@ struct ForwardConfig {
     websocket: Option<WebSocketConnectorConfig>,
 }
 
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+pub struct Route {
+    bv: BitVec,
+}
+
+#[inline]
+pub fn hash24<T: Hash + ?Sized>(v: &T) -> usize {
+    (hash32(&v) >> (32 - 24)) as usize
+}
+
+impl Route {
+    pub fn new() -> Self {
+        
+        let size_24bit:usize = 16777216 - 1;
+        let mut bv = BitVec::from_elem(size_24bit, false);
+
+        if let Ok(lines) = read_lines("rule.txt") {
+            for line in lines {
+                if let Ok(domain) = line {
+                    let i = hash24(&domain);
+                    bv.set(i, true);
+                }
+            }
+        }
+        Self { bv }
+    }
+
+    #[inline]
+    pub fn is_match(&self, domain: &str) -> bool {
+        let hash = hash24(domain);
+        if let Some(true) = self.bv.get(hash) {
+            return true;
+        }
+
+        let len = domain.len();
+        for (i, &item) in domain.as_bytes().iter().enumerate() {
+            if item == b'.' {
+                let str = &domain[i + 1..len];
+                let hash = hash24(str);
+                if let Some(true) = self.bv.get(hash) {
+                    return true;
+                }
+            }
+        }
+        false
+    }
+}
+
+#[inline]
+async fn udp_proxy<I: ProxyUdpStream, O: ProxyConnector + 'static>(inbound: I, connector: Arc<O>) {
+    match connector.connect_udp().await {
+        Ok(outbound) => {
+            relay_udp(inbound, outbound).await;
+        }
+        Err(e) => {
+            log::error!("failed to relay udp connection: {}", e.to_string());
+        }
+    }
+}
+
+#[inline]
+async fn tcp_proxy<I: ProxyTcpStream, O: ProxyConnector + 'static>(
+    inbound: I,
+    connector: Arc<O>,
+    addr: &Address,
+) {
+    match connector.connect_tcp(addr).await {
+        Ok(outbound) => {
+            relay_tcp(inbound, outbound).await;
+        }
+        Err(e) => {
+            log::error!("failed to relay tcp connection: {}", e.to_string());
+        }
+    }
+}
+
 async fn run_proxy<I: ProxyAcceptor, O: ProxyConnector + 'static>(
     acceptor: I,
     connector: O,
@@ -115,17 +205,53 @@ async fn run_proxy<I: ProxyAcceptor, O: ProxyConnector + 'static>(
             Ok(AcceptResult::Tcp((inbound, addr))) => {
                 let connector = connector.clone();
                 smol::spawn(async move {
-                    match connector.connect_tcp(&addr).await {
-                        Ok(outbound) => {
-                            relay_tcp(inbound, outbound).await;
-                        }
-                        Err(e) => {
-                            log::error!(
-                                "failed to relay tcp connection to {}: {}",
-                                addr.to_string(),
-                                e.to_string()
-                            );
-                        }
+                    log::info!("accepted {}", &addr);
+                    tcp_proxy(inbound, connector, &addr).await;
+                })
+                .detach();
+            }
+            Ok(AcceptResult::Udp(inbound)) => {
+                let connector = connector.clone();
+                smol::spawn(async move {
+                    udp_proxy(inbound, connector).await;
+                })
+                .detach();
+            }
+            Err(e) => {
+                log::error!("accept failed: {}", e);
+            }
+        }
+    }
+}
+
+async fn run_rule_proxy<I: ProxyAcceptor, O: ProxyConnector + 'static>(
+    acceptor: I,
+    connector: O,
+) -> io::Result<()> {
+    let connector = Arc::new(connector);
+    let direct_connector = Arc::new(DirectConnector {});
+    let route = Route::new();
+    loop {
+        match acceptor.accept().await {
+            Ok(AcceptResult::Tcp((inbound, addr))) => {
+                //match domain
+                let result: bool;
+                match &addr {
+                    SocketAddress(_sockaddr) => result = false,
+                    DomainNameAddress(domain, _) => result = route.is_match(domain),
+                }
+
+                //do proxy
+                let connector = connector.clone();
+                let direct_connector = direct_connector.clone();
+
+                smol::spawn(async move {
+                    if result == false {
+                        log::info!("accepted {}", &addr);
+                        tcp_proxy(inbound, connector, &addr).await;
+                    } else {
+                        log::info!("directed {}", &addr);
+                        tcp_proxy(inbound, direct_connector, &addr).await;
                     }
                 })
                 .detach();
@@ -133,14 +259,7 @@ async fn run_proxy<I: ProxyAcceptor, O: ProxyConnector + 'static>(
             Ok(AcceptResult::Udp(inbound)) => {
                 let connector = connector.clone();
                 smol::spawn(async move {
-                    match connector.connect_udp().await {
-                        Ok(outbound) => {
-                            relay_udp(inbound, outbound).await;
-                        }
-                        Err(e) => {
-                            log::error!("failed to relay tcp connection: {}", e.to_string());
-                        }
-                    }
+                    udp_proxy(inbound, connector).await;
                 })
                 .detach();
             }
@@ -199,12 +318,12 @@ pub async fn launch_from_config_string(config_string: String) -> io::Result<()> 
             let tls_connector = TrojanTlsConnector::new(&config.tls)?;
             if config.websocket.is_none() {
                 let trojan_connector = TrojanConnector::new(&config.trojan, tls_connector)?;
-                run_proxy(socks5_acceptor, trojan_connector).await?;
+                run_rule_proxy(socks5_acceptor, trojan_connector).await?;
             } else {
                 let ws_connector =
                     WebSocketConnector::new(&config.websocket.unwrap(), tls_connector)?;
                 let trojan_connector = TrojanConnector::new(&config.trojan, ws_connector)?;
-                run_proxy(socks5_acceptor, trojan_connector).await?;
+                run_rule_proxy(socks5_acceptor, trojan_connector).await?;
             }
         }
         "forward" => {
@@ -214,12 +333,12 @@ pub async fn launch_from_config_string(config_string: String) -> io::Result<()> 
             let tls_connector = TrojanTlsConnector::new(&config.tls)?;
             if config.websocket.is_none() {
                 let trojan_connector = TrojanConnector::new(&config.trojan, tls_connector)?;
-                run_proxy(dokodemo_acceptor, trojan_connector).await?;
+                run_rule_proxy(dokodemo_acceptor, trojan_connector).await?;
             } else {
                 let ws_connector =
                     WebSocketConnector::new(&config.websocket.unwrap(), tls_connector)?;
                 let trojan_connector = TrojanConnector::new(&config.trojan, ws_connector)?;
-                run_proxy(dokodemo_acceptor, trojan_connector).await?;
+                run_rule_proxy(dokodemo_acceptor, trojan_connector).await?;
             }
         }
         _ => {
